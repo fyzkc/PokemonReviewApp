765e7882ffe9ac24bfc3c76cc9ff7c5eaf7902d2
Feyza <koc.feyza.fb@gmail.com>
2025-02-11 19:08:47 +0300

    the name property changed into firstname and lastname in owners entity

---
+++ Migrations/20250211160202_RenameNameToFirstAndLast.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReviewApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameNameToFirstAndLast : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Owners",
+                newName: "FirstName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Owners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Owners");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Owners",
+                newName: "Name");
+        }
+    }
+}

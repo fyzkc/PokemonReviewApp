632b2ee04127c62c6b8300f72db23ad266c836dc
Feyza <koc.feyza.fb@gmail.com>
2025-02-13 17:02:54 +0300

    dto folder created and GetById and GetPokemonRating methods created in the controller.

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -23,8 +23,8 @@ namespace PokemonReviewApp.Controllers
         {
             // it calls the GetPokemons method from the repository that we injected in the constructor. 
             var pokemons = _pokemonRepository.GetPokemons();
-            
-            if(!ModelState.IsValid) //it controls if the request is valid or not. 
+
+            if (!ModelState.IsValid) //it controls if the request is valid or not. 
             {
                 return BadRequest(ModelState); // if its not a valid request than it returns BadRequest.
             }
@@ -32,5 +32,37 @@ namespace PokemonReviewApp.Controllers
             return Ok(pokemons); //if its valid, then it returns Ok and the result of GetPokemons method. 
             // it is the list of Pokemons. 
         }
+
+        [HttpGet ("{pokemonId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)] // if its a bad request than its returning the 400 status code. 
+        public IActionResult GetById(int pokemonId) // we will use the GetById method from the PokemonRepository class. 
+        {
+            if (!_pokemonRepository.IfPokemonExists(pokemonId)) // first we are checking the pokemon is whether exists or not. 
+                return NotFound();
+
+            var pokemon = _pokemonRepository.GetById(pokemonId); // then if it can find the pokemon then we are running the GetById mwthod for getting thw details of the pokemon.
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet("{pokemonId}/rating")] //it should be the same name for the url and the method for the variable.
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokemonId) // if it will be a different name from the url's, than it will require 2 variable for id area. and thats not what we want. 
+        {
+            if(!_pokemonRepository.IfPokemonExists(pokemonId))
+                return NotFound();
+
+            var rating = _pokemonRepository.GetPokemonRating(pokemonId);
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(rating);
+        }
+
     }
 }
